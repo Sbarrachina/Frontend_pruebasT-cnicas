5d34d28bbab082ee2c5e701a03f7c537b32d487e
Sbarrachina <sandrabarrachina1978@gmail.com>
2023-03-31 13:56:17 +0200

    agregando ajedrez

--- Basicos/main.js
+++ Basicos/main.js
@@ -6,3 +6,36 @@ for (let i = 1; i <= 7; i++) {
   linea += '#';
   console.log(linea);
 }
+
+// fizzbuzz 
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+// Tablero de ajedrez 
+
+let size = 8; // tamaño de la cuadrícula
+
+let board = ""; // inicializamos la cuadrícula como un string vacío
+
+for (let y = 0; y < size; y++) { // recorremos filas
+  for (let x = 0; x < size; x++) { // recorremos columnas
+    if ((x + y) % 2 === 0) { // si la suma de fila y columna es par, agregamos espacio
+      board += " ";
+    } else { // si no, agregamos #
+      board += "#";
+    }
+  }
+  board += "\n"; // agregamos salto de línea al final de cada fila
+}
+
+console.log(board); // mostramos la cuadrícula en la consola
