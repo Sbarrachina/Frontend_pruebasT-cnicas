3cccc67d5aca73785533fc43bf8f456ba2a6ec3a
Sbarrachina <sandrabarrachina1978@gmail.com>
2023-03-31 13:18:16 +0200

    finalizando fizzbuzz

--- FIZZBUZZ/fizzbuzz.js
+++ FIZZBUZZ/fizzbuzz.js
@@ -1,4 +1,13 @@
 function fizzbuzz(num){
+
+    if(num == 0){
+        return 0;
+    }
+
+    if (num % 3 == 0 && num % 5 ==0){
+        return "fizzbuzz"
+    }
+
     if (num %3==0){
         return "fizz";
     }
@@ -8,4 +17,49 @@ function fizzbuzz(num){
 
     return num;
 }
+
+function print(num) {
+    for (let i = 0; i <= num; i++) {
+        console.log(`${i}: ${fizzbuzz(i)}`);
+    }
+}
+
+print (16)
+
+
 module.exports = fizzbuzz;
+
+// CÓDIGO REFACTORIZADO 
+
+// function fizzbuzz(num){
+
+//     const divisible = (divisor, num) => num % divisor == 0;
+
+//     if(num == 0){
+//         return 0;
+//     }
+
+//     if (divisible(3, num) && divisible(5,num)){
+//         return "fizzbuzz"
+//     }
+
+//     if (divisible (3,num)){
+//         return "fizz";
+//     }
+//     if (divisible(5,num)){
+//         return "buzz";
+//     }
+
+//     return num;
+// }
+
+// function print(num) {
+//     for (let i = 0; i <= num; i++) {
+//         console.log(`${i}: ${fizzbuzz(i)}`);
+//     }
+// }
+
+// print (16)
+
+
+// module.exports = fizzbuzz;
